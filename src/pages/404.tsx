0315f2d4ec4efc8a9c6df0a21965241d8fad3e24
FiezDev <79656850+FiezDev@users.noreply.github.com>
2022-08-11 01:50:31 +0700

    build: implement vscode project settings

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,13 @@
-import { ENUM_EROR_CODE } from '@/model/enum';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { ENUM_EROR_CODE } from "@/model/enum";
+import IndexLayout from "../components/layoutIndex";
 
 type Props = {};
 
-const e404 = () => {
+const e404 = (props: Props) => {
   return (
     <>
       <section className="container mx-auto flex flex-col items-center justify-center h-screen text-2xl md:text-5xl gap-4">
@@ -23,5 +25,5 @@ const e404 = () => {
     </>
   );
 };
-
+e404.layout = IndexLayout;
 export default e404;
