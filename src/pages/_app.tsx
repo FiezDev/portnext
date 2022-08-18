ce5e0b981d28f95fded0ef585faa4e85ab347ed3
FiezDev <79656850+FiezDev@users.noreply.github.com>
2022-08-19 03:42:37 +0700

    fix: remove serviceworker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,12 @@
 import '@/styles/globals.css';
 import { ThemeProvider } from '@material-tailwind/react';
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
 import { NextPageWithLayout } from '../pageWithLayouts';
 
 interface AppPropsWithLayout extends AppProps {
   Component: NextPageWithLayout;
 }
 function App({ Component, pageProps }: AppPropsWithLayout) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/serviceWorker.js').then(
-          function (registration) {
-            console.log(
-              'Service Worker registration successful with scope: ',
-              registration.scope
-            );
-          },
-          function (err) {
-            console.log('Service Worker registration failed: ', err);
-          }
-        );
-      });
-    }
-  }, []);
-
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
