444d5effb46a04e6958db364facb587d0dc196ae
FiezDev <79656850+FiezDev@users.noreply.github.com>
2022-08-18 16:15:56 +0700

    feat: add serviceworker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import { ThemeProvider } from '@material-tailwind/react';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import { NextPageWithLayout } from '../pageWithLayouts';
 
 interface AppPropsWithLayout extends AppProps {
@@ -8,6 +9,24 @@ interface AppPropsWithLayout extends AppProps {
 }
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/serviceWorker.js').then(
+          function (registration) {
+            console.log(
+              'Service Worker registration successful with scope: ',
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err);
+          }
+        );
+      });
+    }
+  }, []);
+
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
