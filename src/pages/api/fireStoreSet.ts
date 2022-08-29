59d0ec46f9f5b72ae1036bd31bdfab22ffc8fdc1
FiezDev <79656850+FiezDev@users.noreply.github.com>
2022-08-29 14:56:02 +0700

    fix: fix api

--- src/pages/api/fireStoreSet.ts
+++ src/pages/api/fireStoreSet.ts
@@ -1,8 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { doc, getFirestore, setDoc } from 'firebase/firestore';
-import config from '../../../config.json';
 
-const firebaseConfig = config.Firebase;
+const firebaseConfig = {
+  apiKey: 'AIzaSyDlX0XlIiTmqBoRq-whTMlVcw6I1U_XnvY',
+  authDomain: 'fiezport.firebaseapp.com',
+  databaseURL:
+    'https://fiezport-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'fiezport',
+  storageBucket: 'fiezport.appspot.com',
+  messagingSenderId: '10093093757',
+  appId: '1:10093093757:web:50d1a301bc34d3aa086214',
+  measurementId: 'G-KK7H46FWTR',
+};
 const app = initializeApp(firebaseConfig);
 const DBref = getFirestore(app);
 
