f60fd98de56fe0e0ef6b46af5f60a820781715f1
FiezDev <79656850+FiezDev@users.noreply.github.com>
2022-09-07 02:35:11 +0700

    refactor: clean index layout

--- src/layouts/layout.Index.tsx
+++ src/layouts/layout.Index.tsx
@@ -1,27 +1,7 @@
-// import Head from 'next/head';
-// import Nav from '@/components/portfolio/Sidenav';
-// import Footer from '@/components/portfolio/Footer';
+export interface IIndexLayout extends React.ComponentPropsWithoutRef<'div'> {}
 
-const name = 'Fiez';
-export const siteTitle = 'Next.js Sample Website';
-
-export interface IIndexLayout extends React.ComponentPropsWithoutRef<'div'> {
-  // justify?: 'items-center' | 'items-start';
-}
-
-const IndexLayout: React.FC<IIndexLayout> = ({
-  children,
-  // justify = 'items-center',
-  // ...divProps
-}) => {
+const IndexLayout: React.FC<IIndexLayout> = ({ children }) => {
   return <div>{children}</div>;
-  //   <Head>
-  //   <title>NextJs Fullstack App Template</title>
-  // </Head>
-  // <div {...divProps} className={`min-h-screen flex flex-col ${justify}`}>
-  //   <Header />
-  //   <main className="px-5">{children}</main>
-  //   <div className="m-auto" />
 };
 
 export default IndexLayout;
