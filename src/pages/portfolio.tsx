ed5e05d6b39cfffd1744aaa74a311e7f114da385
FiezDev <79656850+FiezDev@users.noreply.github.com>
2022-09-07 16:11:05 +0700

    feat: head title add

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -4,11 +4,16 @@ import Main from '@/components/portfolio/Main';
 import Skills from '@/components/portfolio/Skills';
 import Works from '@/components/portfolio/Works';
 import PortfolioLayout from '@/layouts/layout.Portfolio';
+import Head from 'next/head';
 import { NextPageWithLayout } from '../pageWithLayouts';
 
 const Portfolio: NextPageWithLayout = () => {
   return (
     <>
+      <Head>
+        <title>Ittipol Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div id="main">
         <Main />
       </div>
