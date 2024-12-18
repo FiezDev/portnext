71b899fc4bee881e14eca1b4df9f08a6078585c6
FiezDev <79656850+FiezDev@users.noreply.github.com>
2024-12-18 07:50:20 +0700

    remove form

--- components/portfolio/Contact.tsx
+++ components/portfolio/Contact.tsx
@@ -7,7 +7,7 @@ const Contact = () => (
     className="container w-full flex flex-col xl:flex-row items-start justify-center mx-auto p-5 xl:px-[3.75rem] mt-16 md:mt-24 gap-4 text-xl"
   >
     <ContactInfo />
-    <ContactForm />
+    {/* <ContactForm /> */}
   </section>
 );
 
