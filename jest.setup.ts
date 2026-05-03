d35f5b20e242f4679ee7d04c1cb098ae18b2566d
FiezDev <itti.task@gmail.com>
2026-05-03 15:40:35 +0700

    test: add jest + jest.setup with matchMedia stub

---
+++ jest.setup.ts
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+
+// matchMedia stub for jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    addListener: () => {},
+    removeListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
